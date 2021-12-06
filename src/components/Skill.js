3b2206290719bf22bacc9577d396b517ef9495ed
Ashenafi <ashenafibordea@gmail.com>
2021-12-06 15:23:16 -0800

    after back-end app

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,46 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import { Proficient, Familiar } from '../SkillData'
+// import { Proficient, Familiar } from '../SkillData'
 const Skill = () => {
+  const [Proficient, setProficient] = useState([]);
+  const [Familiar, setFamiliar] = useState([]);
+  useEffect(async function getData() {
+    const result1 = await fetch("https://portfolio555.herokuapp.com/skills");
+    result1.json().then((data) => setProficient(data));
+    const result2 = await fetch(
+      "https://asherthechamp.github.io/portfolio-data/data/famSkillsData.json"
+    );
+    result2.json().then((data) => setFamiliar(data));
+  }, []);
+
   return (
     <div id="Skill">
-    <section className='my-28  px-5'>
-      <header className='text-2xl font-bold pt-10 mb-5'>
-        <h2>Skills & Tools</h2>
-      </header>
-      {Proficient?.length > 0 &&
-        <div className='pl-3'>
-          <h2 className='text-xl'> Proficient </h2>
-          <div className='my-7 grid gap-5 grid-cols-3 md:grid-cols-6 content-center'>
-            {Proficient.map((x, index) =>
-              <div className='flex flex-col items-center' tabIndex='0' role='img' aria-label={x.skill} key={index}>
-                <img src={x.logo} alt='' loading='lazy' className='w-16 mb-1' width='64px' height='64px' />
-                {x.skill}
-              </div>
-            )}
-
-
-          </div>
-        </div>
-      }
-      {Familiar?.length > 0 &&
-        <div className='pl-3'>
-          <h2 className='text-xl'> Familiar </h2>
-        <div className='my-7 grid gap-5 grid-cols-3 md:grid-cols-6 content-center'>
-          {Familiar.map((x, index) =>
-            <div className='flex flex-col items-center' tabIndex='0' role='img' aria-label={x.skill} key={index}>
-              <img src={x.logo} alt='' loading='lazy' className='w-16 mb-1' width='64px' height='64px' />
-              {x.skill}
+      <section className="my-28  px-5">
+        <header className="text-2xl font-bold pt-10 mb-5">
+          <h2>Skills & Tools</h2>
+        </header>
+        {Proficient?.length > 0 && (
+          <div className="pl-3 skills">
+            {/* <h2 className="text-xl"> Proficient </h2> */}
+            <div className="my-7 grid gap-5 grid-cols-3 md:grid-cols-6 content-center">
+              {Proficient.map((x, index) => (
+                <div
+                  className="flex flex-col items-center"
+                  tabIndex="0"
+                  role="img"
+                  aria-label={x.skill}
+                  key={index}
+                >
+                  <img
+                    src={x.logo}
+                    alt=""
+                    loading="lazy"
+                    className="w-16 mb-1"
+                    width="64px"
+                    height="64px"
+                  />
+                  {x.skill}
+                </div>
+              ))}
             </div>
-          )}
+          </div>
+        )}
+        {/* {Familiar?.length > 0 && (
+          <div className="pl-3">
+            <h2 className="text-xl"> Familiar </h2>
+            <div className="my-7 grid gap-5 grid-cols-3 md:grid-cols-6 content-center">
+              {Familiar.map((x, index) => (
+                <div
+                  className="flex flex-col items-center"
+                  tabIndex="0"
+                  role="img"
+                  aria-label={x.skill}
+                  key={index}
+                >
+                  <img
+                    src={x.logo}
+                    alt=""
+                    loading="lazy"
+                    className="w-16 mb-1"
+                    width="64px"
+                    height="64px"
+                  />
+                  {x.skill}
+                </div>
+              ))}
+            </div>
+          </div>
+        )} */}
 
-
-        </div>
-
-        </div>
-      }
-      
-      {/* </div> */}
+        {/* </div> */}
       </section>
     </div>
   );
--- src/root.link.js
+++ src/root.link.js
@@ -1,11 +1,14 @@
-export const RESUME_PATH = 'https://drive.google.com/file/d/11mhd52rfA17Qn8hGFw5TyOSahCRDg84z/view?usp=sharing';
-export const GITHUB_USERNAME = 'codewithvk';
-export const LINKEDIN_USERNAME = 'vivekkumarjaviya';
-export const FIRST_NAME = 'Vivekkumar';
-export const LAST_NAME = 'Javiya';
-export const ADDRESS = 'Rajkot, Gujrat'
-export const EMAIL_ID = 'work.vivekjaviya@gmail.com';
-export const TWITTER_ID = 'Vk_javiya';
-export const FRONT_PAGE_NAME = 'Vivek Kumar';
-export const FRONT_PAGE_DESC = 'A Full-Stack Web Developer & Open Source Contributor.I like to build things for web and solving problems.'
-
+export const RESUME_PATH =
+  "https://www.dropbox.com/s/y9ksl4lt2n8pf7e/Ashenafi_New_Resume.docx?dl=0";
+export const GITHUB_USERNAME = "asherthechamp";
+export const LINKEDIN_USERNAME = "asherthechamp";
+export const FIRST_NAME = "Ashenafi";
+export const LAST_NAME = "Bordea";
+export const ADDRESS = "Addis Ababa, Ethiopia";
+export const EMAIL_ID = "ashenafibordea@gmail.com";
+export const TWITTER_ID = "asherthechamp";
+export const FRONT_PAGE_NAME = "Ashenafi Bordea";
+export const FRONT_PAGE_DESC =
+  "A Full-Stack Web Developer.A Tech Fanatic, an Enthusiast and a Lifelong Learner.";
+export const API_KEY =
+  "8b85f11bd431fda47a10200fefe1d0155e08eb028f9da3c79f6e1039a3e7901b";
